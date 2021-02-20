2c42874da72c28f1a36043ee5a9ccbc9beb4f8ea
Axel <axelkr.dev@google.com>
2021-02-20 14:23:28 +0100

    provide observable to subscribe to and internally a subject to publish events to

--- src/Client.ts
+++ src/Client.ts
@@ -1,9 +1,22 @@
 import { Logger } from 'sitka';
+import { Observable, Subject } from 'rxjs';
+import { ObjectEvent } from 'happy-barnacle';
 
 export class Client {
     private logger: Logger;
+    public readonly publishedObjectEvents: Observable<ObjectEvent>;
+    private readonly objectEventSubject: Subject<ObjectEvent>; 
+    private readonly endpoint:string;
 
-    constructor() {
+    constructor(endpoint: string) {
         this.logger = Logger.getLogger({ name: this.constructor.name });
+        this.objectEventSubject = new Subject<ObjectEvent>();
+        this.publishedObjectEvents = this.objectEventSubject;
+        this.endpoint = endpoint;
+    }
+
+    private publish(anEvent: ObjectEvent) {
+        this.logger.debug('publishing an event');
+        this.objectEventSubject.next(anEvent);
     }
 }
--- test/Client.spec.ts
+++ test/Client.spec.ts
@@ -4,8 +4,10 @@ import { expect } from 'chai';
 import { Client } from '../src/Client';
 
 describe('Client', () => {
+	const dummyEndpoint = 'http://endpoint';
+
 	it('should create an instance using its constructor', () => {
-		const example: Client = new Client();
+		const example: Client = new Client(dummyEndpoint);
 		expect(example, 'example should exist').to.exist; // tslint:disable-line:no-unused-expression
 	});
 });
