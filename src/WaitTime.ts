d6d162023313b2ab82e2538d8ea8bf925d7644a6
axelkr <axelkr.dev@gmail.com>
2021-03-17 11:48:15 +0100

    longer wait times over time

--- src/WaitTime.ts
+++ src/WaitTime.ts
@@ -4,22 +4,35 @@ export class WaitTime implements IWaitTime {
     private readonly initialWaitTimeMilliseconds: number;
     private readonly increaseWaitTimeMilliseconds: number;
     private readonly maximalWaitTimeMilliseconds: number;
+    private readonly increaseReallyAfterNRequestedIncreases: number = 2;
+    private requestedIncreases = 0;
+    private currentWaitTime: number;
 
     constructor(initialWaitTimeMilliseconds:number,increaseWaitTimeMilliseconds:number, maximalWaitTimeMilliseconds:number) {
         this.initialWaitTimeMilliseconds = initialWaitTimeMilliseconds;
         this.increaseWaitTimeMilliseconds = increaseWaitTimeMilliseconds;
         this.maximalWaitTimeMilliseconds = maximalWaitTimeMilliseconds;
+        this.reset();
     }
 
     public getWaitTimeMilliseconds(): number {
-        return this.initialWaitTimeMilliseconds;
+        return this.currentWaitTime;
     }
 
     public reset(): void {
-
+        this.currentWaitTime = this.initialWaitTimeMilliseconds;
+        this.requestedIncreases = 0;
     }
 
     public increaseWaitTime(): void {
-
+        this.requestedIncreases = this.requestedIncreases + 1;
+        if ( this.requestedIncreases < this.increaseReallyAfterNRequestedIncreases) {
+            return;
+        }
+        this.requestedIncreases = 0;
+        this.currentWaitTime = this.currentWaitTime + this.increaseWaitTimeMilliseconds;
+        if ( this.currentWaitTime > this.maximalWaitTimeMilliseconds ) {
+            this.currentWaitTime = this.maximalWaitTimeMilliseconds;
+        }
     }
 }
